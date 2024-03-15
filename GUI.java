54c25e2313f6ad44a4c010587db28a06e90f476a
JakubMaliszewski99 <kubek.mal@gmail.com>
2024-03-15 14:45:59 +0100

    Wstępny design GUI

---
+++ GUI.java
@@ -0,0 +1,45 @@
+import java.awt.*;
+
+public class GUI extends Frame{
+    public GUI(){
+        setLayout(new BorderLayout());
+        setTitle("Peer-to-peer czat");
+
+        // Top panel
+        Panel topPanel = new Panel();
+        topPanel.setLayout(new FlowLayout());
+        Label ipLabel = new Label("IP drugiego peera: ");
+        topPanel.add(ipLabel);
+        TextField ipInput = new TextField("XXX.XXX.XXX.XXX");
+        ipInput.setEditable(true);
+        topPanel.add(ipInput);
+        Button connectButton = new Button("Połącz");
+        topPanel.add(connectButton);
+        Label statusLabel = new Label("Połączono z peerem: XXX.XXX.XXX.XXX");
+        topPanel.add(statusLabel);
+
+        add(topPanel, "North");
+
+        // Center panel
+        TextArea chatWindow = new TextArea();
+        chatWindow.setEditable(false);
+        add(chatWindow, "Center");
+
+        // Bottom panel
+        Panel bottomPanel = new Panel();
+        bottomPanel.setLayout(new FlowLayout());
+        TextField messageField = new TextField("Tutaj wpisz wiadomość...                              ");
+        messageField.setSize(50, 400);
+        messageField.setEditable(true);
+        bottomPanel.add(messageField);
+        Button sendButton = new Button("Wyślij");
+        bottomPanel.add(sendButton);
+
+        add(bottomPanel, "South");
+
+
+        setSize(600, 500);
+        setResizable(false);
+        setVisible(true);
+    }
+}
